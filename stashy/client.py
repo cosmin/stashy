40c8ceacdc79b3f97e27cfa1a9f4d46f267199d2
Alex Ruiz <alex.ruiz@scytl.com>
2015-03-18 11:13:44 +0100

    Fixed CREATE BRANCH method

--- stashy/client.py
+++ stashy/client.py
@@ -87,5 +87,6 @@ class StashClient(object):
             data = json.dumps(data)
         return self._session.put(self.url(resource), data, **kw)
 
-    def delete(self, resource, **kw):
-        return self._session.delete(self.url(resource), **kw)
+    def delete(self, resource, data=None, **kw):
+        data = json.dumps(data)
+        return self._session.delete(self.url(resource),data, **kw)
