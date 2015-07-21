82f9b68b84d0d9f0094a1b8bb04957b526087d0a
Lukáš Homza <lhomza@cfh.sk>
2015-07-21 22:26:21 +0200

    my bad, small mixup

--- stashy/permissions.py
+++ stashy/permissions.py
@@ -90,6 +90,9 @@ class Users(ResourceBase, FilteredIterableResource):
         """
         return self._client.delete(self.url(), params=dict(name=user))
 
+class Permissions(ResourceBase):
+    groups = Nested(Groups)
+    users = Nested(Users)
 
 class RepositoryUserPermissions(Permissions):
     def _url_for(self):
@@ -194,41 +197,6 @@ class ProjectPermissions(Permissions):
 
         """
         return self._client.post(self._url_for(permission), params=dict(allow=False))
-
-class RepositoryPermissions(Permissions):
-    def _url_for(self):
-        return self.url().rstrip("/") + "/users"
-
-    @ok_or_error
-    def grant(self, user, permission):
-        """
-        Grant or revoke a repository permission to all users, i.e. set the
-        default permission.
-
-        repository permissions:
-            * REPO_READ
-            * REPO_WRITE
-            * REPO_ADMIN
-
-
-        """
-        return self._client.post(self._url_for(), params=dict(name=user,
-                                                              permission=permission))
-
-    @ok_or_error
-    def revoke(self, user):
-        """
-        Revoke a repository permission from all users, i.e. revoke the default
-        permission.
-
-        repository permissions:
-            * REPO_READ
-            * REPO_WRITE
-            * REPO_ADMIN
-
-        """
-        return self._client.post(self._url_for(), params=dict(name=user))
-
 
 update_doc(Groups.all, """
 Returns groups that have been granted at least one permission.
