b3a43e5013c5ab499608e3b44f84ea0f5917b5c2
Joan Aymà <joanayma.comas@clarivate.com>
2018-07-19 12:29:18 +0200

    add webhooks support

--- stashy/repos.py
+++ stashy/repos.py
@@ -71,6 +71,80 @@ class Hooks(ResourceBase, IterableResource):
         """
         return Hook(item, self.url(item), self._client, self)
 
+class Webhook(ResourceBase):
+    def __init__(self, key, url, client, parent):
+        super(Webhook, self).__init__(url, client, parent)
+        self._key = key
+
+    @response_or_error
+    def get(self):
+        """
+        Retrieve a repository webhook
+        """
+        return self._client.get(self.url())
+
+    @response_or_error
+    def enable(self, configuration=None):
+        """
+        Enable a repository webhook, optionally applying new configuration.
+        """
+        return self._client.put(self.url("/enabled"), data=configuration)
+
+    @response_or_error
+    def disable(self):
+        """
+        Disable a repository webhook
+        """
+        return self._client.delete(self.url("/enabled"))
+
+    @response_or_error
+    def settings(self):
+        """
+        Retrieve the settings for a repository webhook
+        """
+        return self._client.get(self.url("/settings"))
+
+    @response_or_error
+    def configure(self, configuration):
+        """
+        Modify the settings for a repository webhook
+        """
+        return self._client.put(self.url("/settings"), data=configuration)
+
+
+class Webhooks(ResourceBase, IterableResource):
+    def all(self, type=None):
+        """
+        Retrieve webhooks for this repository, optionally filtered by type.
+
+        type: Valid values are PRE_RECEIVE or POST_RECEIVE
+        """
+        params=None
+        if type is not None:
+            params = dict(type=type)
+        return self.paginate("", params=params)
+
+    @response_or_error
+    def create(self, name, url, events=["repo:refs_changed"]):
+        """
+        Create a webhook with the given name and url
+        """
+        return self._client.post(self.url(), data={"name": name,
+                                                   "events": events,
+                                                   "url": url
+                                                   })
+
+    def list(self, type=None):
+        """
+        Convenience method to return a list (rather than iterable) of all elements
+        """
+        return list(self.all(type=type))
+
+    def __getitem__(self, item):
+        """
+        Return a :class:`Webhook` object for operations on a specific hook
+        """
+        return Webhook(item, self.url(item), self._client, self)
 
 class Settings(ResourceBase):
     hooks = Nested(Hooks)
@@ -274,6 +348,7 @@ class Repository(ResourceBase):
 
     pull_requests = Nested(PullRequests, relative_path="/pull-requests")
     settings = Nested(Settings)
+    webhooks = Nested(Webhooks)
     branch_permissions = Nested(BranchPermissions, relative_path=None)
 
 
