a8b67b22e320284a862e5b1148a17d465ad56316
Bruno Oliveira <nicoddemus@gmail.com>
2019-01-24 13:14:03 -0200

    Fix syntax error in setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,6 @@ setup(name='stashy',
         'Programming Language :: Python :: 2.6',  
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',        
-        'Programming Language :: Python :: 3.5,
+        'Programming Language :: Python :: 3.5',
         ]
      )
