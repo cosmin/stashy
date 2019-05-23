13f6b76db659b79e0656bd12c77815a7ebd56c57
Frank Lichtenheld <frank.lichtenheld@sophos.com>
2019-05-23 17:06:08 +0200

    branch_permissions: Update documentation
    And some white-space improvements.

--- stashy/branch_permissions.py
+++ stashy/branch_permissions.py
@@ -8,17 +8,22 @@ API_NAME = 'branch-permissions'
 API_VERSION = '2.0'
 API_OVERRIDE_PATH = '{0}/{1}'.format(API_NAME, API_VERSION)
 
+
 class Matcher(Enum):
-    PATTERN='PATTERN'
-    BRANCH='BRANCH'
-    MODEL_CATEGORY='MODEL_CATEGORY'
-    MODEL_BRANCH='MODEL_BRANCH'
+    """Valid values for the matcher_type for Restriction create/update"""
+    PATTERN = 'PATTERN'
+    BRANCH = 'BRANCH'
+    MODEL_CATEGORY = 'MODEL_CATEGORY'
+    MODEL_BRANCH = 'MODEL_BRANCH'
+
 
 class RestrictionType(Enum):
-    PULL_REQUEST='pull-request-only'
-    FAST_FORWARD='fast-forward-only'
-    NO_DELETES='no-deletes'
-    READ_ONLY='read-only'
+    """Valid values for the restriction_type for Restriction create/update"""
+    PULL_REQUEST = 'pull-request-only'
+    FAST_FORWARD = 'fast-forward-only'
+    NO_DELETES = 'no-deletes'
+    READ_ONLY = 'read-only'
+
 
 class Restriction(ResourceBase):
     def __init__(self, id, url, client, parent):
@@ -40,10 +45,10 @@ class Restriction(ResourceBase):
         return self._client.delete(self.url())
 
     @staticmethod
-    def request_data(match, users, groups, keys, restriction_type, matcher_type):
+    def request_data(match, users, groups, keys, restriction_type,
+                     matcher_type):
         data = dict(type=restriction_type.value)
-        data['matcher'] = dict(type={'id': matcher_type.value},
-                               id=match)
+        data['matcher'] = dict(type={'id': matcher_type.value}, id=match)
 
         if users is not None:
             data['users'] = users
@@ -55,11 +60,19 @@ class Restriction(ResourceBase):
         return data
 
     @response_or_error
-    def update(self, match, users=None, groups=None, keys=None, restriction_type=RestrictionType.READ_ONLY, matcher_type=Matcher.PATTERN):
+    def update(self, match, users=None, groups=None, keys=None,
+               restriction_type=RestrictionType.READ_ONLY,
+               matcher_type=Matcher.PATTERN):
         """
-        Re-restrict a branch, or set of branches defined by a pattern, to a set of users and/or groups
+        Re-restrict a branch, or set of branches defined by a pattern,
+        to a set of users, groups, and access keys.
+        Warning: The REST API does not actually support a direct update of
+        branch permissions. The Restriction will be deleted and recreated instead.
+        Note: access keys need to be specified by their numerical id. labels are
+        not accepted.
         """
-        data = self.request_data(match, users, groups, keys, restriction_type, matcher_type)
+        data = self.request_data(match, users, groups, keys, restriction_type,
+                                 matcher_type)
         self.delete()
         return self._client.put(self.url(""), data=data)
 
@@ -73,16 +86,23 @@ class Restrictions(ResourceBase, IterableResource):
         return Restriction(item, self.url(item), self._client, self)
 
     @response_or_error
-    def create(self, match, users=None, groups=None, keys=None, restriction_type=RestrictionType.READ_ONLY, matcher_type=Matcher.PATTERN):
+    def create(self, match, users=None, groups=None, keys=None,
+               restriction_type=RestrictionType.READ_ONLY,
+               matcher_type=Matcher.PATTERN):
         """
-        Restrict a branch, or set of branches defined by a pattern, to a set of users, groups, and access keys
+        Restrict a branch, or set of branches defined by a pattern,
+        to a set of users, groups, and access keys.
+        Note: access keys need to be specified by their numerical id. labels are
+        not accepted.
         """
         data = Restriction.request_data(match, users, groups, keys,
                                         restriction_type, matcher_type)
 
         return self._client.post(self.url(""), data=data)
 
+
 update_doc(Restrictions.all, """Retrieve list of restrictions for a repo""")
+
 
 class BranchPermissions(ResourceBase):
     """Simple parent resource for this api, to distinguish restrictions from anything else"""
