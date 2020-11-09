dc0b8af5a657f2d07b096b037e18f51d2808f0b8
mupdt <25388474+mupdt@users.noreply.github.com>
2020-11-09 13:27:07 +0000

    Added support for requests.auth
    This enables the use of custom SSO authentication methods (such as Kerberos).

--- stashy/__init__.py
+++ stashy/__init__.py
@@ -2,12 +2,12 @@ __version__ = "0.6"
 
 from .client import Stash
 
-def connect(url, username, password, verify=True):
+def connect(url, username=None, password=None, verify=True, requests_auth=None):
     """Connect to a Stash instance given a username and password.
 
     This is only recommended via SSL. If you are using self-signed certificates,
     you can use verify=False to ignore SSL verifcation.
     """
-    return Stash(url, username, password, verify=verify)
+    return Stash(url, username, password, verify=verify, requests_auth=requests_auth)
 
 __all__ = ['connect']
